e45491ff7ffe9d8bbafdc87f330516761c335334
yathin <yathin@valorpayteh.com>
2021-11-12 12:30:38 +0530

    csvManage dataset

--- controllers/csvManage.js
+++ controllers/csvManage.js
@@ -8,9 +8,11 @@ const convertCsvToJson = path =>
     fs.createReadStream(path)
       .pipe(csv.parse({ headers: true }))
       .on('error', reject)
-      .on('data', row => insures.push(row))
+      .on('data', row => dataSet.push(row))
       .on('end', () => resolve(dataSet));
   });
+
+const dynamicCollectionCreate = () => {};
 
 const upload = async (req, res) => {
   const dbConnect = dbo.getDb().collection('insurence-data');
@@ -18,45 +20,35 @@ const upload = async (req, res) => {
   try {
     if (req.file == undefined) return res.status(400).send('Please upload a CSV file!');
     const insures = await convertCsvToJson(path);
-    dbConnect
-      .insertMany(insures)
-      .then(() => {
-        return res.status(200).send({ message: 'Uploaded the file successfully: ' + req.file.originalname });
-      })
-      .catch(error => {
-        return res.status(500).send({ message: 'Fail to import data into database!', error: error.message });
-      });
+    await dbConnect.insertMany(insures);
+    return res.status(200).send({ message: 'Uploaded the file successfully: ' + req.file.originalname });
   } catch (error) {
     console.log(error);
-    return res.status(500).send({ message: 'Could not upload the file: ' + req.file.originalname });
+    return res.status(500).send({ message: 'Fail to import data into database!', error: error.message });
   }
 };
 
-const getInsures = (req, res) => {
+const getInsures = async (req, res) => {
   const dbConnect = dbo.getDb().collection('insurence-data');
-  dbConnect
-    .find({})
-    .limit(50)
-    .toArray((err, result) => {
-      if (err) return res.status(500).send({ message: err.message || 'Some error occurred while retrieving insures.' });
-      else return res.send(result);
-    });
+  try {
+    const result = await dbConnect.find({}).limit(50).toArray();
+    return res.send(result);
+  } catch (error) {
+    return res.status(500).send({ message: error.message || 'Some error occurred while retrieving insures.' });
+  }
 };
 
-const searchInsures = (req, res) => {
+const searchInsures = async (req, res) => {
   const { userName } = req.body;
   const dbConnect = dbo.getDb().collection('insurence-data');
-  dbConnect
-    .find({ firstname: { $regex: userName } })
-    .limit(50)
-    .toArray()
-    .then(result => {
-      if (result && result.length > 0) return res.status(200).send({ message: `Results Found for '${userName}'`, data: result });
-      else return res.status(500).send({ message: `No results found for '${userName}'` });
-    })
-    .catch(error => {
-      return res.status(500).send({ message: 'Failed to fetch data in database!', error: error.message });
-    });
+  try {
+    const query = { firstname: { $regex: userName } };
+    const result = await dbConnect.find(query).limit(50).toArray();
+    if (result.length === 0) return res.status(404).send({ message: 'No insures found with the given name.' });
+    else return res.status(200).send({ message: `Results Found for '${userName}'`, data: result });
+  } catch (error) {
+    return res.status(500).send({ message: 'Failed to fetch data in database!', error: error.message });
+  }
 };
 
 module.exports = { upload, getInsures, searchInsures };
