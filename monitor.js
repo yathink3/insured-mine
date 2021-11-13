ca0aba23cfb93902f3d3788b7d268476f0d79c88
yathin <yathink3@gmail.com>
2021-11-13 16:19:10 +0530

    serving

--- monitor.js
+++ monitor.js
@@ -17,7 +17,7 @@ pm2.connect(error => {
   setInterval(() => {
     pm2.describe('server', async (errors, scripts) => {
       const server = scripts[0];
-      console.log(`${server.name} - cpu: ${server.monit.cpu}%, memory:${server.monit.memory}`);
+      console.log(`${server.name} - cpu:${server.monit.cpu}% , memory:${server.monit.memory}`);
       if (server.monit.cpu > 70) {
         console.log(`Closing cluster after cpu: ${server.monit.cpu}% usage.`);
         pm2.restart('server', error => {
